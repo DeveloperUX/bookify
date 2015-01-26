b0823c5713a45df8cb896b5ddf2fe9969cbdf72d
Mustafa AbuDarsh <mismael7@gmail.com>
2015-01-25 23:08:15 -0500

    Finally figured out the proper way to call the different controllers, services, and the main module

--- public/app.js
+++ public/app.js
@@ -1,13 +1,1 @@
-//angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
-
-
-function StoryCtrl($scope) {
-  $scope.tagline = 'To the moon and back!';	
-}
-
-function StoryService() {}
-
-angular.module('scribblit', [
-  'ngRoute', 
-  'StoryCtrl'
-]);
+angular.module('Scribblit', []);
